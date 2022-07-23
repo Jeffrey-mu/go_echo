0f860501143e426be870ea35f99a44fea77e05be
Jeffrey <1115378579@qq.com>
2022-07-23 20:32:32 +0800

    feat: post api

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -26,6 +27,7 @@ func main() {
 	})
 	e.GET("/users/:id", getUser)
 	e.GET("/show", show)
+	e.POST("/save", save)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -41,4 +43,41 @@ func show(c echo.Context) error {
 	data := c.QueryParam("data")
 	log.Printf("[DEBUG] User: %v Data:(%v)", name, data)
 	return c.String(http.StatusOK, "name:"+name+", data:"+data)
+}
+
+// func save(c echo.Context) error {
+// 	name := c.FormValue("name")
+// 	email := c.FormValue("email")
+// 	log.Println(name, email)
+// 	return c.String(http.StatusOK, "name:"+name+", email:"+email)
+// }
+func save(c echo.Context) error {
+	// Get name
+	name := c.FormValue("name")
+	// Get avatar
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
 }
